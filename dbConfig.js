7ce6028eefed1c8e0970f8cd763b06436de37ceb
AkankshaThakre <aakankshat@medvol.in>
2024-02-01 08:39:16 +0530

    myFirstCommit

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,34 @@
-const sql = require("msnodesqlv8");
+const { Pool } = require("pg");
 
 require("dotenv").config();
 
-const connectionString = `Server=${process.env.DB_SERVER};Database=${process.env.DB_NAME};Trusted_Connection=${process.env.DB_TRUSTED_CONNECTION};Driver=${process.env.DB_DRIVER}`;
+const pool = new Pool({
+  connectionString: `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
+});
 
-const query = "SELECT * FROM actors";
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS users (
+    id serial PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    email VARCHAR (50) UNIQUE NOT NULL
+  );
+`;
 
-sql.query(connectionString, query, (err, rows) => console.log(rows));
+// Execute the query to create the table
+pool.query(createTableQuery, (error, results) => {
+  if (error) {
+    throw error;
+  }
+  console.log("Table created successfully");
+  // Now you can perform other operations with the database
+  // Close the pool to end the script
+  pool.end();
+});
 
-module.exports = connectionString;
+// const connectionString = `Server=${process.env.DB_SERVER};Database=${process.env.DB_NAME};Trusted_Connection=${process.env.DB_TRUSTED_CONNECTION};Driver=${process.env.DB_DRIVER}`;
+
+// const query = "SELECT * FROM awards";
+
+// sql.query(connectionString, query, (err, rows) => console.log("Hello"));
+
+// module.exports = connectionString;
--- index.js
+++ index.js
@@ -2,41 +2,85 @@ const express = require("express");
 
 const app = express();
 
-const sql = require("msnodesqlv8");
+app.use(express.json());
+
+// const sql = require("msnodesqlv8");
 
 const dbConfig = require("./dbConfig");
-
-const producersData = require("./Assets/producersData.json");
-
-const producers = producersData.producers;
-let sqlQuery = `INSERT INTO producers (ProducerID, ProducerName, BirthDate, CountryID)`;
-
-producers.map((producer, index) => {
-  if (index === producers.length - 1) {
-    sqlQuery =
-      sqlQuery +
-      `SELECT '${producer["ProducerID"]}', '${producer["ProducerName"]}', '${producer["BirthDate"]}', (SELECT CountryID from countries WHERE CountryName LIKE '${producer["CountryName"]}%');`;
-  } else {
-    sqlQuery =
-      sqlQuery +
-      `SELECT '${producer["ProducerID"]}', '${producer["ProducerName"]}', '${producer["BirthDate"]}', (SELECT CountryID from countries WHERE CountryName LIKE '${producer["CountryName"]}%')
-      UNION ALL
-      `;
-  }
-});
 
 const port = 5000;
 
 app.get("/", async (req, res) => {
-  sql.query(dbConfig, sqlQuery, (err, rows) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
-    } else {
-      console.log(rows);
-      res.send("Data stored successfully");
-    }
-  });
+  res.send("Welcome");
 });
+
+// app.get("/movies", async (req, res) => {
+//   let sqlQuery = "SELECT * FROM movies";
+//   sql.query(dbConfig, sqlQuery, (err, rows) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send("Internal Server Error");
+//     } else {
+//       res.status(200).send({
+//         Status: true,
+//         message: "Data retrieved successfully",
+//         result: rows,
+//       });
+//     }
+//   });
+// });
+
+// app.post("/movie", async (req, res) => {
+//   let dataToBeInserted = req.body;
+//   let sqlQuery = `INSERT INTO movies (MovieID, Title, ReleaseDate, Genre, DirectorID, CountryID) VALUES ('${dataToBeInserted["MovieID"]}', '${dataToBeInserted["Title"]}', '${dataToBeInserted["ReleaseDate"]}', '${dataToBeInserted["Genre"]}' ,(SELECT DirectorID FROM directors WHERE DirectorName LIKE '${dataToBeInserted["DirectorName"]}%'),(SELECT CountryID FROM countries WHERE CountryName LIKE '${dataToBeInserted["CountryName"]}%'))`;
+//   sql.query(dbConfig, sqlQuery, (err, rows) => {
+//     if (err) {
+//       res.status(500).send("Internal Server Error");
+//     } else {
+//       res.status(200).send({
+//         status: true,
+//         message: "Data added successfully",
+//         result: [],
+//       });
+//     }
+//   });
+// });
+
+// app.put("/movie", async (req, res) => {
+//   const dataToBeUpdated = req.body;
+//   let sqlQuery = `UPDATE movies SET Title = '${dataToBeUpdated.NewMovieName}' WHERE MovieID = '${dataToBeUpdated.MovieID}'`;
+//   sql.query(dbConfig, sqlQuery, (err, rows) => {
+//     if (err) {
+//       res.status(500).send("Internal Server Error");
+//     } else {
+//       res.status(200).send({
+//         status: true,
+//         message: "Data updated successfully",
+//         result: [],
+//       });
+//     }
+//   });
+// });
+
+// app.delete("/movie", async (req, res) => {
+//   const dataToBeDeleted = req.body;
+//   let sqlQuery = `DELETE FROM movies WHERE MovieID = '${dataToBeDeleted.MovieID}'`;
+//   sql.query(dbConfig, sqlQuery, (err, rows) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send({
+//         status: false,
+//         message: "Failed to delete the data",
+//         error: "err.message",
+//       });
+//     } else {
+//       res.status(200).send({
+//         status: true,
+//         message: "Date deleted successfully",
+//         result: [],
+//       });
+//     }
+//   });
+// });
 
 app.listen(port, () => console.log(`Server is listening at port ${port}`));
