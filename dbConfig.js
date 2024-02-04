20c62a67fc0081c53a044fc0be8c1206e7d3eced
AkankshaThakre <aakankshat@medvol.in>
2024-02-04 11:35:41 +0530

    mssql to postgres shift

--- dbConfig.js
+++ dbConfig.js
@@ -2,33 +2,159 @@ const { Pool } = require("pg");
 
 require("dotenv").config();
 
+const connectionString = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-  connectionString: `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
+  connectionString: connectionString,
 });
 
-const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS users (
-    id serial PRIMARY KEY,
-    username VARCHAR (50) UNIQUE NOT NULL,
-    email VARCHAR (50) UNIQUE NOT NULL
-  );
-`;
+// For 'countries' table
+
+const countriesData = require("./Assets/countriesData.json");
+const countries = countriesData.countries;
+
+let insertDataIntoCountriesQuery =
+  "INSERT INTO countries (CountryID, CountryName) VALUES ";
+
+countries.forEach((obj, index) => {
+  if (index !== countries.length - 1) {
+    insertDataIntoCountriesQuery =
+      insertDataIntoCountriesQuery +
+      ` ('${obj.CountryID}', '${obj.CountryName}'),`;
+  } else {
+    insertDataIntoCountriesQuery =
+      insertDataIntoCountriesQuery +
+      ` ('${obj.CountryID}', '${obj.CountryName}');`;
+  }
+});
+
+// For 'actors' table
+
+const actorsData = require("./Assets/actorsData.json");
+const actors = actorsData.actors;
+
+let insertDataIntoActorsQuery =
+  "INSERT INTO actors (ActorID, ActorName, BirthDate, CountryID) VALUES";
+
+actors.forEach((obj, index) => {
+  if (index !== actors.length - 1) {
+    insertDataIntoActorsQuery =
+      insertDataIntoActorsQuery +
+      ` ('${obj.ActorID}', '${obj.ActorName}', '${obj.BirthDate}', (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%')),`;
+  } else {
+    insertDataIntoActorsQuery =
+      insertDataIntoActorsQuery +
+      ` ('${obj.ActorID}', '${obj.ActorName}', '${obj.BirthDate}', (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%'));`;
+  }
+});
+
+// For 'directors' table
+
+const directorsData = require("./Assets/directorsData.json");
+const directors = directorsData.directors;
+
+let insertDataIntoDirectorsQuery =
+  "INSERT INTO directors (DirectorID, DirectorName, BirthDate, CountryID) VALUES";
+
+directors.forEach((obj, index) => {
+  if (index !== directors.length - 1) {
+    insertDataIntoDirectorsQuery =
+      insertDataIntoDirectorsQuery +
+      ` ('${obj.DirectorID}', '${obj.DirectorName}', '${obj.BirthDate}', (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%')),`;
+  } else {
+    insertDataIntoDirectorsQuery =
+      insertDataIntoDirectorsQuery +
+      ` ('${obj.DirectorID}', '${obj.DirectorName}', '${obj.BirthDate}', (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%'));`;
+  }
+});
+
+// For 'producers' table
+
+const producersData = require("./Assets/producersData.json");
+const producers = producersData.producers;
+
+let insertDataIntoProducersQuery =
+  "INSERT INTO producers (ProducerID, ProducerName, BirthDate, CountryID) VALUES";
+
+producers.forEach((obj, index) => {
+  if (index !== producers.length - 1) {
+    insertDataIntoProducersQuery =
+      insertDataIntoProducersQuery +
+      ` ('${obj.ProducerID}', '${obj.ProducerName}', '${obj.BirthDate}', (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%')),`;
+  } else {
+    insertDataIntoProducersQuery =
+      insertDataIntoProducersQuery +
+      ` ('${obj.ProducerID}', '${obj.ProducerName}', '${obj.BirthDate}', (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%'));`;
+  }
+});
+
+// For 'directors' table: missing data
+
+const missingDirectorsData = require("./Assets/missingDirectorsData.json");
+const missingDirectors = missingDirectorsData.missingDirectors;
+
+let insertMissingDataIntoDirectorsQuery =
+  "INSERT INTO directors (DirectorID, DirectorName, BirthDate, CountryID) VALUES";
+
+missingDirectors.forEach((obj, index) => {
+  if (index !== missingDirectors.length - 1) {
+    insertMissingDataIntoDirectorsQuery =
+      insertMissingDataIntoDirectorsQuery +
+      ` ('${obj.DirectorID}', '${obj.DirectorName}', '${obj.BirthDate}', (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%')),`;
+  } else {
+    insertMissingDataIntoDirectorsQuery =
+      insertMissingDataIntoDirectorsQuery +
+      ` ('${obj.DirectorID}', '${obj.DirectorName}', '${obj.BirthDate}', (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%'));`;
+  }
+});
+
+// For 'movies' table
+
+const moviesData = require("./Assets/moviesData.json");
+const movies = moviesData.movies;
+
+let insertDataIntoMoviesQuery =
+  "INSERT INTO movies (MovieID, Title, ReleaseDate, Genre, DirectorID, CountryID) VALUES";
+
+movies.forEach((obj, index) => {
+  if (index !== movies.length - 1) {
+    insertDataIntoMoviesQuery =
+      insertDataIntoMoviesQuery +
+      ` ('${obj.MovieID}', '${obj.Title}', '${obj.ReleaseDate}', '${obj.Genre}', (SELECT DirectorID from directors WHERE DirectorName LIKE '${obj.DirectorName}%'), (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%')),`;
+  } else {
+    insertDataIntoMoviesQuery =
+      insertDataIntoMoviesQuery +
+      ` ('${obj.MovieID}', '${obj.Title}', '${obj.ReleaseDate}', '${obj.Genre}', (SELECT DirectorID from directors WHERE DirectorName LIKE '${obj.DirectorName}%'), (SELECT CountryID from countries WHERE CountryName LIKE '${obj.CountryName}%'));`;
+  }
+});
+
+// For 'awards' table
+
+const awardsData = require("./Assets/awardsData.json");
+const awards = awardsData.awards;
+
+let insertDataIntoAwardsQuery =
+  "INSERT INTO awards (AwardID, AwardName, Category, Year) VALUES";
+
+awards.forEach((obj, index) => {
+  if (index !== awards.length - 1) {
+    insertDataIntoAwardsQuery =
+      insertDataIntoAwardsQuery +
+      ` ('${obj.AwardID}', '${obj.AwardName}', '${obj.Category}', '${obj.Year}'),`;
+  } else {
+    insertDataIntoAwardsQuery =
+      insertDataIntoAwardsQuery +
+      ` ('${obj.AwardID}', '${obj.AwardName}', '${obj.Category}', '${obj.Year}');`;
+  }
+});
 
 // Execute the query to create the table
-pool.query(createTableQuery, (error, results) => {
+let query = "SELECT * FROM users";
+pool.query(query, (error, results) => {
   if (error) {
     throw error;
   }
-  console.log("Table created successfully");
-  // Now you can perform other operations with the database
-  // Close the pool to end the script
-  pool.end();
+  console.log(results.rows);
 });
 
-// const connectionString = `Server=${process.env.DB_SERVER};Database=${process.env.DB_NAME};Trusted_Connection=${process.env.DB_TRUSTED_CONNECTION};Driver=${process.env.DB_DRIVER}`;
-
-// const query = "SELECT * FROM awards";
-
-// sql.query(connectionString, query, (err, rows) => console.log("Hello"));
-
-// module.exports = connectionString;
+module.exports = { pool };
--- index.js
+++ index.js
@@ -1,86 +1,86 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-// const sql = require("msnodesqlv8");
+const { pool } = require("./dbConfig");
 
-const dbConfig = require("./dbConfig");
-
-const port = 5000;
+const port = 5000 || 80 || 443;
 
 app.get("/", async (req, res) => {
   res.send("Welcome");
 });
 
-// app.get("/movies", async (req, res) => {
-//   let sqlQuery = "SELECT * FROM movies";
-//   sql.query(dbConfig, sqlQuery, (err, rows) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send("Internal Server Error");
-//     } else {
-//       res.status(200).send({
-//         Status: true,
-//         message: "Data retrieved successfully",
-//         result: rows,
-//       });
-//     }
-//   });
-// });
+app.get("/movies", async (req, res) => {
+  let sqlQuery = "SELECT * FROM movies";
+  pool.query(sqlQuery, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.status(200).send({
+        Status: true,
+        message: "Data retrieved successfully",
+        result: results.rows,
+      });
+    }
+  });
+});
 
-// app.post("/movie", async (req, res) => {
-//   let dataToBeInserted = req.body;
-//   let sqlQuery = `INSERT INTO movies (MovieID, Title, ReleaseDate, Genre, DirectorID, CountryID) VALUES ('${dataToBeInserted["MovieID"]}', '${dataToBeInserted["Title"]}', '${dataToBeInserted["ReleaseDate"]}', '${dataToBeInserted["Genre"]}' ,(SELECT DirectorID FROM directors WHERE DirectorName LIKE '${dataToBeInserted["DirectorName"]}%'),(SELECT CountryID FROM countries WHERE CountryName LIKE '${dataToBeInserted["CountryName"]}%'))`;
-//   sql.query(dbConfig, sqlQuery, (err, rows) => {
-//     if (err) {
-//       res.status(500).send("Internal Server Error");
-//     } else {
-//       res.status(200).send({
-//         status: true,
-//         message: "Data added successfully",
-//         result: [],
-//       });
-//     }
-//   });
-// });
+app.post("/movie", async (req, res) => {
+  let dataToBeInserted = req.body;
+  let sqlQuery = `INSERT INTO movies (MovieID, Title, ReleaseDate, Genre, DirectorID, CountryID) VALUES ('${dataToBeInserted["MovieID"]}', '${dataToBeInserted["Title"]}', '${dataToBeInserted["ReleaseDate"]}', '${dataToBeInserted["Genre"]}' ,(SELECT DirectorID FROM directors WHERE DirectorName LIKE '${dataToBeInserted["DirectorName"]}%'),(SELECT CountryID FROM countries WHERE CountryName LIKE '${dataToBeInserted["CountryName"]}%'))`;
+  pool.query(sqlQuery, (err, results) => {
+    if (err) {
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.status(200).send({
+        status: true,
+        message: "Data added successfully",
+        result: [],
+      });
+    }
+  });
+});
 
-// app.put("/movie", async (req, res) => {
-//   const dataToBeUpdated = req.body;
-//   let sqlQuery = `UPDATE movies SET Title = '${dataToBeUpdated.NewMovieName}' WHERE MovieID = '${dataToBeUpdated.MovieID}'`;
-//   sql.query(dbConfig, sqlQuery, (err, rows) => {
-//     if (err) {
-//       res.status(500).send("Internal Server Error");
-//     } else {
-//       res.status(200).send({
-//         status: true,
-//         message: "Data updated successfully",
-//         result: [],
-//       });
-//     }
-//   });
-// });
+app.put("/movie", async (req, res) => {
+  const dataToBeUpdated = req.body;
+  let sqlQuery = `UPDATE movies SET Title = '${dataToBeUpdated.NewMovieName}' WHERE MovieID = '${dataToBeUpdated.MovieID}'`;
+  pool.query(sqlQuery, (err, results) => {
+    if (err) {
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.status(200).send({
+        status: true,
+        message: "Data updated successfully",
+        result: [],
+      });
+    }
+  });
+});
 
-// app.delete("/movie", async (req, res) => {
-//   const dataToBeDeleted = req.body;
-//   let sqlQuery = `DELETE FROM movies WHERE MovieID = '${dataToBeDeleted.MovieID}'`;
-//   sql.query(dbConfig, sqlQuery, (err, rows) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send({
-//         status: false,
-//         message: "Failed to delete the data",
-//         error: "err.message",
-//       });
-//     } else {
-//       res.status(200).send({
-//         status: true,
-//         message: "Date deleted successfully",
-//         result: [],
-//       });
-//     }
-//   });
-// });
+app.delete("/movie", async (req, res) => {
+  const dataToBeDeleted = req.body;
+  let sqlQuery = `DELETE FROM movies WHERE MovieID = '${dataToBeDeleted.MovieID}'`;
+  pool.query(sqlQuery, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send({
+        status: false,
+        message: "Failed to delete the data",
+        error: "err.message",
+      });
+    } else {
+      res.status(200).send({
+        status: true,
+        message: "Date deleted successfully",
+        result: [],
+      });
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Server is listening at port ${port}`));
